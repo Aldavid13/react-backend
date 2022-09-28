bf07536914943432580de3e4ede726265d346b3e
pingui <pingui@pingui.com>
2022-09-28 08:42:32 -0400

    modificacion codigo java

--- src/main/java/com/example/demo/VariableController.java
+++ src/main/java/com/example/demo/VariableController.java
@@ -4,10 +4,9 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -36,6 +35,23 @@ public class VariableController {
     }
 
 
+
+    @GetMapping(value = "api/imprimirTexto", produces = "application/json")
+    public ResponseEntity<Map<String,String>> imprimirTexto() {
+        Map<String,String> valor = new HashMap<>();
+
+        String value = System.getenv("texto");
+        if (value == null || System.getenv("texto").isEmpty())
+            value = "No texto estas enviando el texto como variable de entorno";
+        else if(System.getenv("nombre")==null || System.getenv("nombre").isEmpty())
+            value ="No estas colocando la variable nombre";
+        else if(!value.toLowerCase().contains(System.getenv("nombre").toLowerCase())){
+            value=" No estas reemplazando bien tu nombre en el archivo: \n \n  ESTAS IMPRIMIENDO: \n \n"+ value;
+        }
+        valor.put("data",value);
+        return new ResponseEntity<>(valor, HttpStatus.OK);
+    }
+}
 
     @GetMapping(value = "api/imprimirTexto", produces = "application/json")
     public ResponseEntity<Map<String,String>> imprimirTexto() {
